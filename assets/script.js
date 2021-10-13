4f2ef6653c26923b8b3072d9c35287b170443b83
Brian Helms <bhelms2132@gmail.com>
2021-10-13 12:14:27 -0400

    Loop created and button variable

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,12 @@
 // Array for sounds
 const sounds = ['applause','boo','gasp','tada','victory','wrong'];
+
+// Looping through Array
+sounds.forEach(sound => {
+    // Button variable
+    const btn = document.createElement('button')
+    // Class 
+    btn.classList.add('btn')
+
+
+})
