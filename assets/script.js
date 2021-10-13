7b78a1794b51e06a0aa13dffd2aa77d0d37567a1
Brian Helms <bhelms2132@gmail.com>
2021-10-13 12:54:33 -0400

    styling added to the body and font imported

--- assets/script.js
+++ assets/script.js
@@ -4,9 +4,12 @@ const sounds = ['applause','boo','gasp','tada','victory','wrong'];
 // Looping through Array
 sounds.forEach(sound => {
     // Button variable
-    const btn = document.createElement('button')
+    const btn = document.createElement('button');
     // Class 
-    btn.classList.add('btn')
+    btn.classList.add('btn');
 
+    btn.innerText = sound;
+
+    document.getElementById('buttons').appendChild(btn)
 
 })
